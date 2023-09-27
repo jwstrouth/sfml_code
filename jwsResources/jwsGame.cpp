c7db6e64876643e7081583605aebdad34cd64e8b
James Strouth <31066967+jwstrouth@users.noreply.github.com>
2023-09-27 14:16:29 -0500

    moved files out library into application folder

---
+++ jwsResources/jwsGame.cpp
@@ -0,0 +1,69 @@
+#include "jwsGame.h"
+
+jwsGame::jwsGame():
+m_window(sf::VideoMode(640, 480), "JWS Resources")
+{
+
+}
+
+jwsGame::~jwsGame()
+{
+    //dtor
+}
+
+void jwsGame::ProcessEvents()
+{
+    sf::Event event;
+    while(m_window.pollEvent(event))
+    {
+        switch(event.type)
+        {
+            case sf::Event::Closed:
+                m_window.close();
+                break;
+        }
+    }
+}
+
+void jwsGame::Update()
+{
+
+}
+
+void jwsGame::Render()
+{
+    m_window.clear();
+
+    m_window.draw(*m_landscape);
+    m_window.draw(*m_airplane);
+
+    m_window.display();
+}
+
+void jwsGame::Run()
+{
+    try
+	{
+		m_textures.Load(Textures::Landscape, "Desert.png");
+        m_textures.Load(Textures::Airplane, "Eagle.png");
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+		return;
+	}
+
+	sf::Sprite landscape(m_textures.Get(Textures::Landscape));
+	sf::Sprite airplane(m_textures.Get(Textures::Airplane));
+	airplane.setPosition(200.f, 200.f);
+
+	m_landscape = &landscape;
+	m_airplane = &airplane;
+
+    while(m_window.isOpen())
+    {
+        ProcessEvents();
+        Update();
+        Render();
+    }
+}
---
+++ jwsResources/jwsGame.h
@@ -0,0 +1,45 @@
+#ifndef JWSGAME_H
+#define JWSGAME_H
+
+#include <SFML/Graphics.hpp>
+
+#include <jwsResourceHolder.h>
+
+#include <iostream>
+using namespace std;
+
+// Resource ID for sf::Texture
+namespace Textures
+{
+	enum ID
+	{
+		Landscape,
+		Airplane,
+	};
+}
+
+class jwsGame: private sf::NonCopyable
+{
+    public:
+        jwsGame();
+        virtual ~jwsGame();
+
+        void Run();
+
+    protected:
+
+    private:
+        void ProcessEvents();
+        void Update();
+        void Render();
+
+    private:
+        sf::RenderWindow                                m_window;
+        //sf::Texture                                     m_texture;
+        sf::Sprite                                      m_player;
+        jwsResourceHolder<sf::Texture, Textures::ID>    m_textures;
+        sf::Sprite                                      *m_landscape;
+        sf::Sprite                                      *m_airplane;
+};
+
+#endif // JWSGAME_H
